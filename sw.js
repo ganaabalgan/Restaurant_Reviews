1a3e19712984b95935e67ae69052eaaec1b47f60
gb015j <gb015j@att.com>
2018-11-22 12:37:28 -0800

    fixed breadcrumb accessability and css

--- sw.js
+++ sw.js
@@ -47,8 +47,13 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
 	event.respondWith(
 		caches.match(event.request)
-		.then(function(response) {
-			return response || fetch(event.request);
-		})
+			.then(function(response) {
+				// Cache hit - return response
+				if (response) {
+					return response;
+				}
+				return fetch(event.request);
+			}
+		)
 	);
 });
